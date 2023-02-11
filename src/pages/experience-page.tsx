d68d386a03ae81426fc43fad34d49a2e87dd3c7c
kukjinkim <tlaruddo2@gmail.com>
2023-02-10 22:58:50 -0800

    Add draft of expereince page

--- src/pages/experience-page.tsx
+++ src/pages/experience-page.tsx
@@ -1,4 +1,27 @@
-import { Box } from "@mui/material";
+import { Box,  Typography } from "@mui/material";
 export const ExperiencePage = () => {
-  return <Box>expereince Page</Box>;
+  return (
+    <Box sx={{
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column"
+    }}>
+      <Typography variant="h4"> EXPERIENCE PAGE</Typography>
+      <Box sx={{
+        display:"flex",
+        flexDirection: "row"
+      }}>
+        <Box 
+          component={"img"}
+          sx={{
+          height:"10%",
+          width: "30%"
+        }}
+          src="https://intersystemcontrols.com/wp-content/uploads/2022/12/ISC-Logo-scaled-1.webp"
+          alt="intersystem controls logo"
+        />
+        <Typography variant="h6"> Intersystem Controls</Typography>
+      </Box>
+    </Box>
+  );
 };
