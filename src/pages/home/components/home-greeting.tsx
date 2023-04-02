f3729252888396d2a882354b1dbd37fcf2a18275
kukjinkim <tlaruddo2@gmail.com>
2023-04-02 00:05:32 -0700

    Add responsive design in home

--- src/pages/home/components/home-greeting.tsx
+++ src/pages/home/components/home-greeting.tsx
@@ -1,28 +1,17 @@
 import { Link } from "react-router-dom"
-import styled from 'styled-components';
+import { Button } from "../styled/button.styles"
+import { GreetingContainer } from "../styled/greeting-container"
 
-const Button = styled.button`
-    background-color: #9370DB;
-    border: none;
-    color: #FFFFFF;
-    text-align: center;
-    font-size: 20px;
-    padding: 10px;
-    border-radius: 12px;
-    margin: 10px 0 0 0;
-`;
 
 
 export const HomeGreeting = () => { 
     return ( 
-        <div style={{width: "40vw", padding:20}}>
-            <h1 style={{padding: 10}}>Hello World!</h1>
-            <div style={{padding: 10}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque accusantium maxime beatae adipisci, excepturi quis dolorem repudiandae doloribus ad modi harum esse quod tempore et sit voluptatibus reprehenderit perspiciatis.</div>
+        <GreetingContainer>
+            <h1 style={{ padding: 10 }}>Hello World!</h1>
+            <div style={{ padding: 10 }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque accusantium maxime beatae adipisci, excepturi quis dolorem repudiandae doloribus ad modi harum esse quod tempore et sit voluptatibus reprehenderit perspiciatis.</div>
             <Link to={`/my-website/project`} style={{ textDecoration: "none", color: "black", padding: 10}}>
-                    <Button>
-                        Go to 'About'
-                    </Button>
+                    <Button> Who am I ? </Button>
             </Link>
-        </div>
+        </GreetingContainer>
     )
 }
---
+++ src/pages/home/styled/animation-container.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const AnimationContainer = styled.div`
+    width: 80vw;
+    height: 300;
+
+    @media screen and (min-width: 750px){
+        width: 40vw;
+        height: 500;
+    }
+
+`
