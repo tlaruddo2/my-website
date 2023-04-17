4fd7568f3e6d48603c8da64e212ae1fa98f2bf16
kukjinkim <tlaruddo2@gmail.com>
2023-04-16 23:40:35 -0700

    test API with notion

--- client/src/pages/project-page.tsx
+++ client/src/pages/project-page.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const fetchData = () => { 
   fetch("http://localhost:8000/")
@@ -5,12 +6,22 @@ const fetchData = () => {
     .then(res => console.log(res))
 }
 
-export const ProjectPage = () => { 
+export const ProjectPage = () => {
+  const [res, setRes] = useState("");
+
+  const fetchData = () => {
+    fetch("http://localhost:8000/")
+      .then( res => res.json()) //JSON.parse(res)
+      .then( res => JSON.stringify(res))
+      .then( res => setRes(res))
+  }
+  
   return (
     <>
       <button onClick={fetchData}>
         data
       </button>
+      <p>{res}</p>
     </>
   )
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,42 +1,51 @@
 require("dotenv").config();
-import http from "http"; 
-import { Client } from "@notionhq/client"; 
+import http from "http";
+import { Client } from "@notionhq/client";
 
-// const notionDatabaseId = process.env.NOTION_DATABASE_ID;
-// const notionToken = process.env.NOTION_TOKEN;
-const notionDatabaseId = "1c0101b33ea04cbca65f26207db609c5"
-const notionToken ="secret_9VmcnB2AyNLBC6aTzBBZZFI3j9vjgjDC9Q7yoRYXwbA"
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+const notionToken = process.env.NOTION_TOKEN;
 
 if (!notionDatabaseId || !notionToken){
-    throw Error("Must define notion toekn and databse id in env");
+    throw Error("Must define notion token and database id in env");
 }
 
-const notion = new Client({
-    auth: notionToken,
-})
+const notion = new Client({auth: notionToken});
 
+const host = "localhost"
+const port = 8000;
 
-const host = "localhost";
-const port = 8000; 
-
-const server = http.createServer(async (req, res) => {
+const server = http.createServer( async (req, res) => {
+    //* Avoid CORS error
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader("Content-Type", "application/json");
-    
+
     switch(req.url){
-        case "/": 
+        case "/":
+            const query = await notion.databases.query({ database_id: notionDatabaseId });
+
+            //
+            const data = query.results.map((row) => {
+                if ("properties" in row){
+                    console.log(row.properties);
+
+                }
+                
+
+
+            })
+            // console.log(data);
+
+
             res.writeHead(200);
-            const data = await notion.databases.query({database_id: notionDatabaseId})
-            console.log("data", data);
-            res.end(JSON.stringify({data: data}));
+            res.end(JSON.stringify({ data: query }))
             break;
         
         default: 
-            res.writeHead(404);
-            res.end(JSON.stringify({ error: "Resource not found"}))            
+            res.writeHead(400);
+            res.end(JSON.stringify({ error: "Resource not found" }))
     }
 })
 
-server.listen(port, host, () => {
+server.listen( port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 })
